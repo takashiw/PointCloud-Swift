16524faae0f44ac48dabb1401f429440dd2ab91d
cailhuiris <cailhuiris@gmail.com>
2016-02-08 03:23:46 +0900

    Add Resampling Function

--- PointClouds/PointCloud.swift
+++ PointClouds/PointCloud.swift
@@ -2,6 +2,45 @@ import UIKit
 
 class PointCloud {
     let origin = Point(x:0.0, y:0.0, id:0)
+    let numPoints = 32
+    
+    
+    // var keyword in function parameter makes it to be mutable.
+    // resampling given points
+    func resample(var points:[Point]) -> [Point] {
+        let intervalLength = pathLength(points) / Double(numPoints - 1)
+        var distance = 0.0
+        var newPoints = [Point](count: numPoints, repeatedValue: Point(x: 0.0, y:0.0, id:0))
+        newPoints[0] = Point(x: points[0].x, y:points[0].y, id:points[0].id)
+        var k = 1;
+        
+        for i in 1..<points.count {
+            let p1 = points[i]
+            let p2 = points[i-1]
+            if(p1.id == p2.id) {
+                let d = getDistance(p2, p2:p1)
+                if((distance + d) >= intervalLength){
+                    let qx = p2.x + (((intervalLength - distance) / d) * (p1.x - p2.x))
+                    let qy = p2.y + (((intervalLength - distance) / d) * (p1.y - p2.y))
+                    let q = Point(x: qx, y: qy, id: p1.id)
+                    
+                    if(k < numPoints){
+                        newPoints[k] = q
+                        // 'q point' will be next p2
+                        points[i] = q
+                        k += 1
+                    }
+                } else {
+                    distance += d
+                }
+            }
+        }
+        
+        let lastPoint = points.last!
+        newPoints[numPoints-1] = Point(x:lastPoint.x, y:lastPoint.y, id:lastPoint.id)
+        
+        return newPoints
+    }
     
     // get the center point from points.
     func centroid(points:[Point]) -> Point {
@@ -28,7 +67,7 @@ class PointCloud {
             let p2 = points[index-1]
             
             if(p1.id == p2.id){
-                d += distance(p1, p2:p2)
+                d += getDistance(p2, p2:p1)
             }
         }
         
@@ -42,7 +81,7 @@ class PointCloud {
             var _d = 0.0
             
             for index in (0..<points1.count) {
-                _d += distance(points1[index], p2: points2[index])
+                _d += getDistance(points1[index], p2: points2[index])
             }
             
             return _d / Double(points1.count)
@@ -50,7 +89,7 @@ class PointCloud {
     }
  
     // get distance between two points.
-    func distance(p1:Point, p2:Point) -> Double {
+    func getDistance(p1:Point, p2:Point) -> Double {
         let dx = p2.x - p1.x
         let dy = p2.y - p1.y
         return sqrt(dx * dx + dy * dy)
